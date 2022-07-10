c973a3afd8c10303c96c2f698dc8ab8fbb8d77fe
temptado <jason1287712@gmail.com>
2022-07-10 19:30:13 -0400

    added swiper script to scroll through images

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,17 +39,17 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader)
 
 // /*==================== SWIPER DISCOVER ====================*/
-// let swiper = new Swiper(".discover__container", {
-//     effect: "coverflow",
-//     grabCursor: true,
-//     centeredSlides: true,
-//     slidesPerView: "auto",
-//     loop: true,
-//     spaceBetween: 32,
-//     coverflowEffect: {
-//         rotate: 0,
-//     },
-// })
+let swiper = new Swiper(".discover__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
 
 // /*==================== VIDEO ====================*/
 // const videoFile = document.getElementById('video-file'),
