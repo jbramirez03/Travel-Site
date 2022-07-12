0dd5df396516cb687e25618a85b8e0424745a6cb
temptado <jason1287712@gmail.com>
2022-07-11 22:18:12 -0400

    added script to show scroll up button

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,12 +85,12 @@ videoFile.addEventListener('ended', finalVideo)
 
 
 // /*==================== SHOW SCROLL UP ====================*/
-// function scrollUp() {
-//     const scrollUp = document.getElementById('scroll-up');
-//     // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-//     if (this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-// }
-// window.addEventListener('scroll', scrollUp)
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 // /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 // const sections = document.querySelectorAll('section[id]')
